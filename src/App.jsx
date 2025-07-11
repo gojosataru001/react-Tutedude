fad8d27b7249437e78065b77afe568a8620c2e5d
gojosataru001 <sharmahunny16197@gmail.com>
2025-07-11 10:41:28 +0530

    added new file Employee.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@
 // import Welcome from './components/Welcome'
 // import Hello from './components/Hello'
 // import HelloMessage from './components/HelloMessage'
-import Class from './components/Class'
-import Student from './components/Student'
+// import Class from './components/Class'
+// import Student from './components/Student'
+import Employee from './components/Employee'
 // let a = "rohan"
 
 function App() {
   
 
   return (
-    <>
+    <div className='bg-blue-600 text-white'>
       {/* <Greet />
       <Welcome />
       <Hello />
@@ -22,12 +23,15 @@ function App() {
       </HelloMessage>
       <HelloMessage name="Sonu" age="12">
         <p>children component 3</p>
-      </HelloMessage> */}
-      {/* <Class  name={a} age="15"/>
+      </HelloMessage>
+      <Class  name={a} age="15"/>
       <Class  name="Ashvin" age="45"/>
-      <Class  name="Virat" age="52"/> */}
-      <Student name="Ajay"/>
-    </>
+      <Class  name="Virat" age="52"/>
+      <Student name="Ajay"/> */}
+
+      
+      <Employee />
+    </div>
   )
 }
 
---
+++ src/components/Employee.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+class Employee extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            name:'Rohan'
+        }
+    }
+    changeName(){
+        this.setState({
+            name:'Rahul'
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h1>{this.state.name}</h1>
+                <button onClick={() => this.changeName()}>Click</button>
+            </div>
+        )
+    }
+}
+export default Employee;
